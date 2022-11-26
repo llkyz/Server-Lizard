3587f0298757db4c7f83454972b2a03b442a9696
llky <lesterlohky@gmail.com>
2022-11-26 18:56:19 +0800

    Update
    Refined infraction and usernotes interfaces (hopefully foolproof)

--- commandList/infraction.py
+++ commandList/infraction.py
@@ -32,7 +32,7 @@ def setup(client):
                 try:
                     user = await client.fetch_user(userId)
                 except:
-                    await ctx.reply(embed=discord.Embed(title='Syntax Error', description='Please follow this format: `!infraction add [userID]` (e.g userID = 1032276665092538489)'), delete_after=60)
+                    await ctx.reply(embed=discord.Embed(title='Syntax Error', description='Please follow this format: `!infraction [@user]` or `!infraction [userID]` (e.g userID = 1032276665092538489)'), delete_after=60)
                 else:
                     targetMessage = ""
                     async for message in profileChannel.history(limit=None):
@@ -45,30 +45,30 @@ def setup(client):
                         embed = discord.Embed(title=f'Profile can\'t be found?! Contact your local lizard support')
                         await ctx.send(embed=embed)
                     else:
+                        infractionStart = targetMessage.embeds[0].description.index('**Infractions**:') + 16
+                        infractions = message.embeds[0].description[infractionStart:]
+                        infractionList = infractions.split('\n* ')
+                        infractionList.pop(0)
+
                         view = discord.ui.View()
                         button1 = discord.ui.Button(label="Add", style=ButtonStyle.green, custom_id='add')
                         button2 = discord.ui.Button(label="Remove", style=ButtonStyle.red, custom_id='remove')
                         button3 = discord.ui.Button(label="Done", style=ButtonStyle.gray, custom_id='done')
                         view.add_item(item=button1)
-                        view.add_item(item=button2)
+                        if len(infractionList) != 0:
+                            view.add_item(item=button2)
                         view.add_item(item=button3)
-                        msg1 = await ctx.reply(content="Viewing Infractions", embed=embed, view=view)
+                        msg1 = await ctx.reply(content="**Viewing Infractions**", embed=embed, view=view)
 
                         def checkButton(m):
                             return m.message == msg1 and m.user == ctx.author
                         try:
                             interacted = await client.wait_for('interaction', timeout=300, check=checkButton)
                         except asyncio.TimeoutError:
-                            view.remove_item(item=button1)
-                            view.remove_item(item=button2)
-                            view.remove_item(item=button3)
-                            await msg1.edit(content='Timed out!', view=view)
+                            await msg1.edit(content='Timed out!', view=None)
                         else:
                             await interacted.response.defer()
-                            view.remove_item(item=button1)
-                            view.remove_item(item=button2)
-                            view.remove_item(item=button3)
-                            await msg1.edit(view=view)
+                            await msg1.edit(view=None)
                             
                             if interacted.data['custom_id'] == 'done':
                                 return
@@ -95,49 +95,41 @@ def setup(client):
                                     await ctx.send(embed=embed)
 
                             elif interacted.data['custom_id'] == 'remove':
-                                infractionStart = targetMessage.embeds[0].description.index('**Infractions**:') + 16
-                                infractions = message.embeds[0].description[infractionStart:]
-                                if len(infractions) < 2:
-                                    await ctx.send(embed=discord.Embed(title=f'No infractions logged'))
+                                def makeOptions(myList):
+                                    return discord.SelectOption(label=f'Infraction #{myList[0]}', value=f'{myList[0]}', description=f'{myList[1]}')
+
+                                options = list(map(makeOptions, list(enumerate(infractionList, start=1))))
+                                options.append(discord.SelectOption(label=f'Cancel', value=f'0'))
+                                view = discord.ui.View()
+                                myMenu = discord.ui.Select(placeholder="Choose an infraction to remove", options=options)
+                                view.add_item(myMenu)
+
+                                msg1 = await ctx.send(view=view)
+
+                                def checkButton(m):
+                                    return m.message == msg1 and m.user == ctx.author
+                                try:
+                                    interacted = await client.wait_for('interaction', timeout=300, check=checkButton)
+                                except asyncio.TimeoutError:
+                                    await msg1.edit(content='Timed out!', view=None)
                                 else:
-                                    infractionList = infractions.split('\n* ')
-                                    embed = discord.Embed(title=f'Current Infractions')
-                                    infractionList.pop(0)
-                                    count = 1
-                                    for x in infractionList:
-                                        embed.add_field(name=f'Infractions #{count}', value=f'> {x}', inline=False)
-                                        count += 1
-                                    await ctx.send(embed=embed)
+                                    await interacted.response.defer()
+                                    optionSelected = interacted.data["values"][0]
+                                    await msg1.delete()
 
-                                    msg2 = await ctx.send(embed=discord.Embed(title=f'Please enter the # of the infraction you wish to remove'))
-
-                                    def checkMessage(m):
-                                        return m.channel == ctx.channel and m.author == ctx.author
-                                    while True:
-                                        try:
-                                            interacted = await client.wait_for('message', timeout=300, check=checkMessage)
-                                        except asyncio.TimeoutError:
-                                            await msg2.edit(content='Timed out!')
-                                            break
-                                        else:
-                                            try:
-                                                infractionNum = int(interacted.content) - 1
-                                                if infractionNum < 0:
-                                                    await ctx.send(embed=discord.Embed(title=f'Invalid number entered'), delete_after=20)
-                                                else:
-                                                    infractionList.pop(infractionNum)
-                                                    revisedInfractionList = ""
-                                                    for x in infractionList:
-                                                        revisedInfractionList += "\n* "
-                                                        revisedInfractionList += x
-                                                    embed = discord.Embed(title=targetMessage.embeds[0].title, description=targetMessage.embeds[0].description[:infractionStart] + revisedInfractionList)
-                                                    embed.set_thumbnail(url=user.display_avatar.url)
-                                                    edited = await targetMessage.edit(embed=embed)
-                                                    embed = discord.Embed(title=edited.embeds[0].title, description=edited.embeds[0].description)
-                                                    embed.set_thumbnail(url=user.display_avatar.url)
-                                                    await ctx.send(embed=embed)
-                                                    break
-                                            except:
-                                                await ctx.send(embed=discord.Embed(title=f'Invalid number entered'), delete_after=20)
+                                    if optionSelected == '0':
+                                        await ctx.send(embed=discord.Embed(title=f'Infraction removal cancelled'))
+                                    else:
+                                        infractionList.pop(int(optionSelected)-1)
+                                        revisedInfractionList = ""
+                                        for x in infractionList:
+                                            revisedInfractionList += "\n* "
+                                            revisedInfractionList += x
+                                        embed = discord.Embed(title=targetMessage.embeds[0].title, description=targetMessage.embeds[0].description[:infractionStart] + revisedInfractionList)
+                                        embed.set_thumbnail(url=user.display_avatar.url)
+                                        edited = await targetMessage.edit(embed=embed)
+                                        embed = discord.Embed(title=edited.embeds[0].title, description=edited.embeds[0].description)
+                                        embed.set_thumbnail(url=user.display_avatar.url)
+                                        await ctx.send(content=f'**Removed Infraction #{optionSelected}**', embed=embed)
         else:
             await ctx.send(embed=discord.Embed(title=f'You do not have permission to use that command'), delete_after=20)
--- commandList/usernotes.py
+++ commandList/usernotes.py
@@ -31,7 +31,7 @@ def setup(client):
                 try:
                     user = await client.fetch_user(userId)
                 except:
-                    await ctx.reply(embed=discord.Embed(title='Syntax Error', description='Please follow this format: `!infraction add [userID]` (e.g userID = 1032276665092538489)'), delete_after=60)
+                    await ctx.reply(embed=discord.Embed(title='Syntax Error', description='Please follow this format: `!usernotes [@user]` or `!usernotes [userID]` (e.g userID = 1032276665092538489)'), delete_after=60)
                 else:
                     targetMessage = ""
                     async for message in profileChannel.history(limit=None):
@@ -44,30 +44,31 @@ def setup(client):
                         embed = discord.Embed(title=f'Profile can\'t be found?! Contact your local lizard support')
                         await ctx.send(embed=embed)
                     else:
+                        notesStart = targetMessage.embeds[0].description.index('**Notes**:') + 10
+                        notesEnd = message.embeds[0].description.index('**Infractions**:') - 1
+                        notes = message.embeds[0].description[notesStart:notesEnd]
+                        noteList = notes.split('\n* ')
+                        noteList.pop(0)
+
                         view = discord.ui.View()
                         button1 = discord.ui.Button(label="Add", style=ButtonStyle.green, custom_id='add')
                         button2 = discord.ui.Button(label="Remove", style=ButtonStyle.red, custom_id='remove')
                         button3 = discord.ui.Button(label="Done", style=ButtonStyle.gray, custom_id='done')
                         view.add_item(item=button1)
-                        view.add_item(item=button2)
+                        if len(noteList) != 0:
+                            view.add_item(item=button2)
                         view.add_item(item=button3)
-                        msg1 = await ctx.reply(content="Viewing User Notes", embed=embed, view=view)
+                        msg1 = await ctx.reply(content="**Viewing User Notes**", embed=embed, view=view)
 
                         def checkButton(m):
                             return m.message == msg1 and m.user == ctx.author
                         try:
                             interacted = await client.wait_for('interaction', timeout=300, check=checkButton)
                         except asyncio.TimeoutError:
-                            view.remove_item(item=button1)
-                            view.remove_item(item=button2)
-                            view.remove_item(item=button3)
-                            await msg1.edit(content='Timed out!', view=view)
+                            await msg1.edit(content='Timed out!', view=None)
                         else:
                             await interacted.response.defer()
-                            view.remove_item(item=button1)
-                            view.remove_item(item=button2)
-                            view.remove_item(item=button3)
-                            await msg1.edit(view=view)
+                            await msg1.edit(view=None)
                             
                             if interacted.data['custom_id'] == 'done':
                                 return
@@ -94,50 +95,41 @@ def setup(client):
                                     await ctx.send(embed=embed)
 
                             elif interacted.data['custom_id'] == 'remove':
-                                notesStart = targetMessage.embeds[0].description.index('**Notes**:') + 10
-                                notesEnd = message.embeds[0].description.index('**Infractions**:') - 1
-                                notes = message.embeds[0].description[notesStart:notesEnd]
-                                if len(notes) < 2:
-                                    await ctx.send(embed=discord.Embed(title=f'No notes logged'))
+                                def makeOptions(myList):
+                                    return discord.SelectOption(label=f'Note #{myList[0]}', value=f'{myList[0]}', description=f'{myList[1]}')
+
+                                options = list(map(makeOptions, list(enumerate(noteList, start=1))))
+                                options.append(discord.SelectOption(label=f'Cancel', value=f'0'))
+                                view = discord.ui.View()
+                                myMenu = discord.ui.Select(placeholder="Choose a note to remove", options=options)
+                                view.add_item(myMenu)
+
+                                msg1 = await ctx.send(view=view)
+
+                                def checkButton(m):
+                                    return m.message == msg1 and m.user == ctx.author
+                                try:
+                                    interacted = await client.wait_for('interaction', timeout=300, check=checkButton)
+                                except asyncio.TimeoutError:
+                                    await msg1.edit(content='Timed out!', view=None)
                                 else:
-                                    noteList = notes.split('\n* ')
-                                    embed = discord.Embed(title=f'Current Notes')
-                                    noteList.pop(0)
-                                    count = 1
-                                    for x in noteList:
-                                        embed.add_field(name=f'Note #{count}', value=f'> {x}', inline=False)
-                                        count += 1
-                                    await ctx.send(embed=embed)
+                                    await interacted.response.defer()
+                                    optionSelected = interacted.data["values"][0]
+                                    await msg1.delete()
 
-                                    msg2 = await ctx.send(embed=discord.Embed(title=f'Please enter the # of the note you wish to remove'))
-
-                                    def checkMessage(m):
-                                        return m.channel == ctx.channel and m.author == ctx.author
-                                    while True:
-                                        try:
-                                            interacted = await client.wait_for('message', timeout=300, check=checkMessage)
-                                        except asyncio.TimeoutError:
-                                            await msg2.edit(content='Timed out!')
-                                            break
-                                        else:
-                                            try:
-                                                noteNum = int(interacted.content) - 1
-                                                if noteNum < 0:
-                                                    await ctx.send(embed=discord.Embed(title=f'Invalid number entered'), delete_after=20)
-                                                else:
-                                                    noteList.pop(noteNum)
-                                                    revisedNoteList = ""
-                                                    for x in noteList:
-                                                        revisedNoteList += "\n* "
-                                                        revisedNoteList += x
-                                                    embed = discord.Embed(title=targetMessage.embeds[0].title, description=targetMessage.embeds[0].description[0:notesStart] + revisedNoteList + targetMessage.embeds[0].description[notesEnd:])
-                                                    embed.set_thumbnail(url=user.display_avatar.url)
-                                                    edited = await targetMessage.edit(embed=embed)
-                                                    embed = discord.Embed(title=edited.embeds[0].title, description=edited.embeds[0].description)
-                                                    embed.set_thumbnail(url=user.display_avatar.url)
-                                                    await ctx.send(embed=embed)
-                                                    break
-                                            except:
-                                                await ctx.send(embed=discord.Embed(title=f'Invalid number entered'), delete_after=20)
+                                    if optionSelected == '0':
+                                        await ctx.send(embed=discord.Embed(title=f'Note removal cancelled'))
+                                    else:
+                                        noteList.pop(int(optionSelected)-1)
+                                        revisedNoteList = ""
+                                        for x in noteList:
+                                            revisedNoteList += "\n* "
+                                            revisedNoteList += x
+                                        embed = discord.Embed(title=targetMessage.embeds[0].title, description=targetMessage.embeds[0].description[0:notesStart] + revisedNoteList + targetMessage.embeds[0].description[notesEnd:])
+                                        embed.set_thumbnail(url=user.display_avatar.url)
+                                        edited = await targetMessage.edit(embed=embed)
+                                        embed = discord.Embed(title=edited.embeds[0].title, description=edited.embeds[0].description)
+                                        embed.set_thumbnail(url=user.display_avatar.url)
+                                        await ctx.send(content=f'**Removed Note #{optionSelected}**', embed=embed)
         else:
             await ctx.send(embed=discord.Embed(title=f'You do not have permission to use that command'), delete_after=20)
