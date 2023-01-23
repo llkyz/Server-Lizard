338a6c191d63d70aea88cce4263fae54eb7ed4b9
llky <lesterlohky@gmail.com>
2023-01-23 18:29:16 +0800

    Update report.py
    Report function improved. Added option for self-harm reports

--- commandList/messageCommandList/report.py
+++ commandList/messageCommandList/report.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from functions import *
+import asyncio
 
 
 def setup(client):
@@ -10,22 +11,54 @@ def setup(client):
         channelData = sqlCursor.fetchone()[0]
 
         if channelData != None:
-            reportChannel = client.get_channel(channelData) #user profiles channel
-            report = ReportModal(title="Report Form")
-            await ctx.send_modal(report)
-            await report.wait()
-            embed = discord.Embed(title=f'__**Post report by {ctx.user.display_name} ({ctx.user})**__', description=f'**Reported User**: {str(message.author.display_name)} ({message.author})\n**Channel**: #{str(message.channel)}\n**Time**: ' + timeConvert(message.created_at) + f'\n**Message Link**: [\[Link\]]({message.jump_url})', color=0xFF5733)
-            embed.add_field(name="Message Content", value=f"> `{message.content}`", inline=False)
-            embed.add_field(name="Report Details", value=report.children[0].value, inline=False)
-            embed.set_footer(text=timeNow())
-            if message.attachments:
-                embed.set_image(url=message.attachments[0].url)
-            if report.children[1].value != "":
-                additionalInfo = report.children[1].value
-            else:
-                additionalInfo = 'N/A'
-            embed.add_field(name="Additional Info", value=additionalInfo, inline=False)
-            await reportChannel.send(embed=embed)
+            reportChannel = client.get_channel(channelData) #reports channel
+
+            view = discord.ui.View()
+            options = []
+            options.append(discord.SelectOption(label=f'Send a written report', value=f'modal'))
+            options.append(discord.SelectOption(label=f'Report message for self-harm', value=f'selfHarm'))
+            options.append(discord.SelectOption(label=f'Cancel', value=f'0'))
+            myMenu = discord.ui.Select(placeholder="Select an option", options=options)
+            view.add_item(myMenu)
+
+            msg1 = await ctx.respond(view=view, ephemeral=True, delete_after=300)
+
+            def checkButton(m):
+                return m.application_id == msg1.application_id and m.user == ctx.author
+            try:
+                interacted = await client.wait_for('interaction', timeout=300, check=checkButton)
+            except asyncio.TimeoutError:
+                await msg1.edit(content='Timed out!', view=None)
+                return
+            
+            await msg1.delete_original_response()
+            optionSelected = interacted.data["values"][0]
+            
+            if optionSelected == 'modal':
+                report = ReportModal(title="Report Form")
+                await interacted.response.send_modal(report)
+                await report.wait()
+                embed = discord.Embed(title=f'__Message Report__', description=f'**Sent by**: {ctx.user.display_name} (`{ctx.user}`)\n**Reported User**: {str(message.author.display_name)} (`{message.author}`)\n**Channel**: #{str(message.channel)}\n**Time**: ' + timeConvert(message.created_at) + f'\n**Message Link**: [\[Link\]]({message.jump_url})', color=0xFF5733)
+                embed.add_field(name="Message Content", value=f"> `{message.content}`", inline=False)
+                embed.add_field(name="Report Details", value=report.children[0].value, inline=False)
+                embed.set_footer(text=timeNow())
+                if message.attachments:
+                    embed.set_image(url=message.attachments[0].url)
+                if report.children[1].value != "":
+                    additionalInfo = report.children[1].value
+                else:
+                    additionalInfo = 'N/A'
+                embed.add_field(name="Additional Info", value=additionalInfo, inline=False)
+                await reportChannel.send(embed=embed)
+
+            elif optionSelected == 'selfHarm':
+                embed = discord.Embed(title=f'__Self-harm Report__', description=f'**Sent by**: {ctx.user.display_name} (`{ctx.user}`)\n**Reported User**: {str(message.author.display_name)} (`{message.author}`)\n**Channel**: #{str(message.channel)}\n**Time**: ' + timeConvert(message.created_at) + f'\n**Message Link**: [\[Link\]]({message.jump_url})', color=0xFF5733)
+                embed.add_field(name="Message Content", value=f"> `{message.content}`", inline=False)
+                embed.set_footer(text=timeNow())
+                if message.attachments:
+                    embed.set_image(url=message.attachments[0].url)
+                await reportChannel.send(embed=embed)
+                await interacted.response.send_message("Thank you, your report has been sent.", ephemeral=True, delete_after=20)
 
         else:
             await ctx.respond("This feature has not been activated.", ephemeral=True, delete_after=20)
@@ -38,5 +71,5 @@ def setup(client):
             self.add_item(discord.ui.InputText(label="Additional supporting links/messages", style=discord.InputTextStyle.long, required=False))
 
         async def callback(self, interaction: discord.Interaction):
-            await interaction.response.send_message('Report received.', ephemeral=True, delete_after=30)
+            await interaction.response.send_message('Thank you, your report has been sent.', ephemeral=True, delete_after=30)
 
