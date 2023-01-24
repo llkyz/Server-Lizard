4765b73d3b181af4d398ae0e1790921da90831cd
llky <lesterlohky@gmail.com>
2023-01-25 00:09:15 +0800

    Update admin.py

--- commandList/admin.py
+++ commandList/admin.py
@@ -20,41 +20,42 @@ def setup(client):
     @client.command() #!admin
     async def admin(ctx, arg=None):
         if not hasAdminRole(ctx) and not checkOwner(ctx):
+            return
         
-            categories = {
-                "admin-features": {'field': "⭐ **Features**", 'modules':[]},
-                "admin-messages": {'field': "📰 **Message Management**", 'modules':[]},
-                "admin-administrative": {'field': "👓 **Administrative**", 'modules':[]},
-                "admin-disciplinary": {'field': "❌ **Disciplinary**", 'modules':[]},
-            }
+        categories = {
+            "admin-features": {'field': "⭐ **Features**", 'modules':[]},
+            "admin-messages": {'field': "📰 **Message Management**", 'modules':[]},
+            "admin-administrative": {'field': "👓 **Administrative**", 'modules':[]},
+            "admin-disciplinary": {'field': "❌ **Disciplinary**", 'modules':[]},
+        }
 
-            if arg == None:
-                commandList = glob.glob("commandList/*.py")
-                for x in commandList:
-                    slice = x.replace("\\", ".").replace("/", ".").replace(".py", "")
-                    commandName = slice.replace("commandList.","")
-                    getDocs = importlib.import_module(slice)
-                    if getDocs.docs["category"] in categories:
-                        categories[getDocs.docs["category"]]["modules"].append(f'`{commandName}`')
+        if arg == None:
+            commandList = glob.glob("commandList/*.py")
+            for x in commandList:
+                slice = x.replace("\\", ".").replace("/", ".").replace(".py", "")
+                commandName = slice.replace("commandList.","")
+                getDocs = importlib.import_module(slice)
+                if getDocs.docs["category"] in categories:
+                    categories[getDocs.docs["category"]]["modules"].append(f'`{commandName}`')
 
-                embed=discord.Embed(title=f'Admin commands for Server Lizard', description='To use a command, type: `![command]`.\nTo get additional info on a command, type: `!admin [commmand name]`',color=0x14AB49)
-                for key in categories:
-                    embed.add_field(name=categories[key]["field"], value=" ".join(categories[key]["modules"]), inline=False)
-                await ctx.reply(embed=embed)
+            embed=discord.Embed(title=f'Admin commands for Server Lizard', description='To use a command, type: `![command]`.\nTo get additional info on a command, type: `!admin [commmand name]`',color=0x14AB49)
+            for key in categories:
+                embed.add_field(name=categories[key]["field"], value=" ".join(categories[key]["modules"]), inline=False)
+            await ctx.reply(embed=embed)
 
-            else:
-                try:
-                    mymodule = "commandList."
-                    getDocs = importlib.import_module(mymodule + arg)
-                    if getDocs.docs["category"] not in categories:
-                        await ctx.reply("Command not found!")
-                    else:
-                        if len(getDocs.docs["aliases"]) == 0:
-                            aliasList = "None"
-                        else:
-                            aliasList = ", ".join(getDocs.docs["aliases"])
-
-                        embed = discord.Embed(title=f'[Command] !{arg}', description=f'**Aliases**: {aliasList}\n\n**Usage**: {getDocs.docs["usage"]}\n\n**Description**\n> {getDocs.docs["description"]}', color=0xaacbeb)
-                        await ctx.reply(embed=embed)
-                except:
+        else:
+            try:
+                mymodule = "commandList."
+                getDocs = importlib.import_module(mymodule + arg)
+                if getDocs.docs["category"] not in categories:
                     await ctx.reply("Command not found!")
+                else:
+                    if len(getDocs.docs["aliases"]) == 0:
+                        aliasList = "None"
+                    else:
+                        aliasList = ", ".join(getDocs.docs["aliases"])
+
+                    embed = discord.Embed(title=f'[Command] !{arg}', description=f'**Aliases**: {aliasList}\n\n**Usage**: {getDocs.docs["usage"]}\n\n**Description**\n> {getDocs.docs["description"]}', color=0xaacbeb)
+                    await ctx.reply(embed=embed)
+            except:
+                await ctx.reply("Command not found!")
